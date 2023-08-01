bdd69ae04a446920ef8497702557f353a4638b2e
Jozef <Jozefw@sbcglobal.net>
2023-08-01 16:57:46 -0700

    add routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
+import {createBrowserRouter,RouterProvider} from 'react-router-dom';
+import HomePage from './Components/HomePage';
+import RQPage from './Components/RQPage';
+import Test from './Components/Test';
 import './App.css';
+
+const browserRouter = createBrowserRouter([
+{path:'/', element:<HomePage></HomePage>},
+{path:'/rq-super-heroes',element:<RQPage></RQPage>},
+{path:'/test', element:<Test></Test>}
+])
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+      <RouterProvider router={browserRouter}>
+      </RouterProvider>
   );
 }
 
